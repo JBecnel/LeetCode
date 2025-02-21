048af2e42ceecb9ec05a05c2c2facd4a82759f0c
JBecnel <becnel@gmail.com>
2025-02-20 19:57:25 -0600

    TreeNode.py

--- TreeNode.py
+++ TreeNode.py
@@ -4,3 +4,39 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+        
+    def insertValues(self, data : list[int]):
+        self.val = data[0]
+        for i in range(1,len(data)):
+            self.insert(data[i])
+        
+    def insert(self, data):
+        if data < self.val:
+            if self.left is None:
+                self.left = TreeNode(data)
+            else:
+                self.left.insert(data)
+        elif data > self.val:
+            if self.right is None:
+                self.right = TreeNode(data)
+            else:
+                self.right.insert(data)
+            
+    def inorderTraversal(self) -> list[int]:
+        if self is None:
+            return []
+        else:
+            res = [ ]
+            if not (self.left is None):
+                res = self.left.inorderTraversal()
+            res.append(self.val)
+            if not (self.right is None):
+                res = res + self.right.inorderTraversal()
+            return res
+        
+def printTree(node : TreeNode, level=0):
+    if node != None:
+        printTree(node.left, level + 1)
+        print(' ' * 4 * level + '-> ' + str(node.val))
+        printTree(node.right, level + 1)
+
